1c3a61a53792852f6bbb2136d032902c90fb6551
rnfkaus8 <kth4350@gmail.com>
2022-01-04 20:18:37 +0900

    add : 태태나 스터디 프로젝트 springboot 연습용 첫 배포

---
+++ src/main/java/taetaena/com/chibbo/ChibboApplication.java
@@ -0,0 +1,13 @@
+package taetaena.com.chibbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChibboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChibboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taetaena/com/chibbo/ChibboApplicationTests.java
@@ -0,0 +1,13 @@
+package taetaena.com.chibbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChibboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
