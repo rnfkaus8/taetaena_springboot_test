438f67f0ba869289bd235b9927ab6a5d534bd596
rnfkaus8 <kth4350@gmail.com>
2022-01-06 23:24:02 +0900

    add : Member Repository 추가

---
+++ src/main/java/taetaena/com/chibbo/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package taetaena.com.chibbo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import taetaena.com.chibbo.domain.Member;
+
+public interface MemberRepository {
+	
+	Member save(Member memeber);
+	Optional<Member> findById(Long id);
+	Optional<Member> findByname(String name);
+	List<Member> findAll();
+	
+	
+	
+}
---
+++ src/main/java/taetaena/com/chibbo/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package taetaena.com.chibbo.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import taetaena.com.chibbo.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+	
+	private static Map<Long, Member> store = new HashMap<Long, Member>();
+	private static long sequence = 0L;
+	
+	@Override
+	public Member save(Member member) {
+		member.setId(++sequence);
+		store.put(member.getId(), member);
+		return member;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		return Optional.ofNullable(store.get(id));
+	}
+
+	@Override
+	public Optional<Member> findByname(String name) {
+		return store.values().stream()
+				.filter(member -> member.getName().equals(name))
+				.findAny();
+	}
+
+	@Override
+	public List<Member> findAll() {
+		return new ArrayList<Member>(store.values());
+	}
+	
+	public void clearStore() {
+		store.clear();
+	}
+
+}
