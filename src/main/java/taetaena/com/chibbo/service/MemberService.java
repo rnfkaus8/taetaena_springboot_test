dbb1fef04cb7cf07bbf51ddad2cea31063a6665a
rnfkaus8 <kth4350@gmail.com>
2022-01-07 17:00:33 +0900

    refac : MemberService Di 추가 (MemberRepository)

---
+++ src/main/java/taetaena/com/chibbo/service/MemberService.java
@@ -0,0 +1,49 @@
+package taetaena.com.chibbo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import taetaena.com.chibbo.domain.Member;
+import taetaena.com.chibbo.repository.MemberRepository;
+
+public class MemberService {
+	
+	private final MemberRepository memberRepository;
+	
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+		
+	}
+	
+	/*
+	 * 회원 가입
+	 */
+	public Long join(Member member) {
+		
+		vaildateDuplicateMember(member); // 중복회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+	
+	// 같은 이름이 있는 회원은 중복 회원으로 칭함
+	// 중복회원이 있으면 안됨
+	private void vaildateDuplicateMember(Member member) {
+		memberRepository.findByname(member.getName())
+				.ifPresent(m -> {
+					throw new IllegalStateException("이미 존재하는 회원입니다.");
+				});
+	}
+	
+	/*
+	 * 전체 회원 조희
+	 */
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	public Optional<Member> findOne(Long MemberId){
+		return memberRepository.findById(MemberId);
+	}
+	
+}
