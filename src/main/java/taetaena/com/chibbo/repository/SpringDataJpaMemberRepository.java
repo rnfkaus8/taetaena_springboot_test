5e6a24482715f2ff225c8476743644c6b0558559
rnfkaus8 <kth4350@gmail.com>
2022-01-09 21:57:55 +0900

    add : springDataJpa Repository

---
+++ src/main/java/taetaena/com/chibbo/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package taetaena.com.chibbo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import taetaena.com.chibbo.domain.Member;
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+	
+	@Override
+	Optional<Member> findByName(String name);
+	
+}
