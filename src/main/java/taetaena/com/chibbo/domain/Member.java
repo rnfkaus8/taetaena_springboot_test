05ab292a265b7f0a803112f705aa55e82e7cd17b
rnfkaus8 <kth4350@gmail.com>
2022-01-09 21:41:02 +0900

    refac : Member domain Entitiy 어노테이션 추가

--- src/main/java/taetaena/com/chibbo/domain/Member.java
+++ src/main/java/taetaena/com/chibbo/domain/Member.java
@@ -1,8 +1,18 @@
 package taetaena.com.chibbo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	
 	public Long getId() {
