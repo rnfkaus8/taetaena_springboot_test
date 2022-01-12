ed5e5f8d6e7ca39309a9b40fd4451c8492f158c1
rnfkaus8 <kth4350@gmail.com>
2022-01-12 20:33:39 +0900

    add : 메소드 별 실행시간 출력 AOP 추가

--- src/main/java/taetaena/com/chibbo/SpringConfig.java
+++ src/main/java/taetaena/com/chibbo/SpringConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import taetaena.com.chibbo.repository.MemberRepository;
-import taetaena.com.chibbo.repository.MemoryMemberRepository;
 import taetaena.com.chibbo.service.MemberService;
 
 @Configuration
@@ -20,6 +19,7 @@ public class SpringConfig {
 	public MemberService memberService() {
 		return new MemberService(memberRepository);
 	}
+	
 
 //	@Bean
 //	public MemberRepository memberRepository() {
